4098b4a6e5214c059fdcf7132eda53c8d04a8858
jzagal <zagalvelozo@gmail.com>
2017-10-24 12:21:43 -0300

    Update cliente.php

--- cliente.php
+++ cliente.php
@@ -44,4 +44,24 @@ socket_write ($socket, "\r\n", strlen ("\r\n"));
 echo "Cerrando socket...";
 socket_close($socket);
 echo "OK.\n\n";
+
+class workerThread extends Thread {
+public function __construct($i){
+  $this->i=$i;
+}
+
+public function run(){
+  while(true){
+   echo $this->i;
+   sleep(1);
+  }
+}
+}
+
+for($i=0;$i<50;$i++){
+$workers[$i]=new workerThread($i);
+$workers[$i]->start();
+}
+
+?>
 ?>

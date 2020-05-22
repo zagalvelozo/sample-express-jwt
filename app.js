0b7a364401ac759e602c65e2a522b2a569e65c15
zagalvelozo <zagalvelozo@gmail.com>
2020-05-22 14:06:58 -0400

    Keep it simple son

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var http = require('http'),
+    path = require('path'),
+    methods = require('methods'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    cors = require('cors'),
+    passport = require('passport'),
+    errorhandler = require('errorhandler'),
+    cookieParser = require('cookie-parser'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    mongoose = require('mongoose');
+
+var isProduction = process.env.NODE_ENV === 'production';
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
+app.use(cors());
+
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
@@ -22,8 +37,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({ secret: 'MyCookie', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
+
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+if(isProduction){
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect('mongodb://localhost/MyDB');
+  mongoose.set('debug', true);
+}
+
+require('./models/User');
+require('./config/passport');
+
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,10 +67,30 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+if (!isProduction) {
+  app.use(function(err, req, res, next) {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({'errors': {
+      message: err.message,
+      error: err
+    }});
+  });
+}
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({'errors': {
+    message: err.message,
+    error: {}
+  }});
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Index' });
 });
+
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'register' });
+});
+
+router.use('/api', require('./api'));
 
 module.exports = router;
